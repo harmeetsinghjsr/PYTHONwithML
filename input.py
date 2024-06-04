bb4416c2278cfda0841d85cb20954c5505fde17d
Harmeet <hs978698@gmail.com>
2024-06-05 00:33:49 +0530

    python with ml first program input 2 numbers and print the sum

---
+++ input.py
@@ -0,0 +1,9 @@
+# Prompt the user to enter a number
+num1 = int(input("Enter a number: "))
+
+# Add the entered number to another number
+num2 = int(input("Enter another number: "))
+result = num1 + num2
+
+# Print the result
+print("The sum is:", result)
