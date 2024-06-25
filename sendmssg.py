1478632ac876ac6f077be05ff4737d116eaea48d
Harmeet <hs978698@gmail.com>
2024-06-25 23:17:13 +0530

    edited

--- sendmssg.py
+++ sendmssg.py
@@ -1,2 +1,2 @@
 import pywhatkit
-pywhatkit.sendwhatmsg("+919031668700", "im yours big fan sir please i want to sponser you can you collab wid me ??", 22, 5 );
+pywhatkit.sendwhatmsg("+91xxxxxxxxxx", "message", 12,0 );
